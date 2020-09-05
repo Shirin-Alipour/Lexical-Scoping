0480e6a265c1550217a46ee1577b860d2963a9cc
shirincc <alipour.shirin@gmail.com>
2020-09-05 15:23:58 -0700

    Assignment-2-Lexical-Scoping

---
+++ Assignment-2.R
@@ -0,0 +1,32 @@
+#Create the new special "matrix" object by returning a list
+
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) i <<- solve
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+#the function attempts to retrieve an inverse from the matrix.
+#If the inverse is not available computes and caches the inverse of the matrix
+
+cacheSolve<- function(x, ...) {
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+}
+
